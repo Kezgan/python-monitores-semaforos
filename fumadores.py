0b525d75675deba5642f447c809cfd52ce00d6b1
Kezgan <lautaromcasella@gmail.com>
2020-10-27 20:04:09 -0300

    Fumadores con semaforos completo, sin bonus

--- fumadores.py
+++ fumadores.py
@@ -1,53 +1,65 @@
 import random
 import threading
 import time
+import logging
 
-papelEnMesa = False
-fosforosEnMesa = False
-tabacoEnMesa = False
+logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+
+semaforoAgente = threading.Semaphore(0)
+semaforoFumadorConPapel = threading.Semaphore(0)
+semaforoFumadorConTabaco = threading.Semaphore(0)
+semaforoFumadorConFosforos = threading.Semaphore(0)
 
 def agente():
-    global papelEnMesa, fosforosEnMesa, tabacoEnMesa
     while True:
         caso = random.choice([0,1,2]) #al azar pone dos cosas en la mesa
         if caso == 0:
-            papelEnMesa = True
-            tabacoEnMesa = True
+            logging.info('Pongo tabaco y fosforos sobre la mesa')
+            time.sleep(2)
+            semaforoFumadorConPapel.release()
         if caso == 1:
-            papelEnMesa = True
-            fosforosEnMesa = True
+            logging.info('Pongo papel y fosforos sobre la mesa')
+            time.sleep(2)
+            semaforoFumadorConTabaco.release()
         if caso == 2:
-            fosforosEnMesa = True
-            tabacoEnMesa = True
+            logging.info('Pongo papel y tabaco sobre la mesa')
+            time.sleep(2)
+            semaforoFumadorConFosforos.release()
+        semaforoAgente.acquire()
         # esperar a reponer las cosas una vez que alguien haya tomado las dos anteriores
 
 def fumadorConPapel():
     while True:
-        # si hay fósforos y tabaco en la mesa
-            # tomarlos
-            # armar cigarrillo y fumar: se puede simular con un sleep
-            # llamar de nuevo a agente para que reponga en la mesa dos cosas al azar
-
-
-def fumadorConFosforos():
-    while True:
-        # si hay papel y tabaco en la mesa
-            # tomarlos
-            # armar cigarrillo y fumar: se puede simular con un sleep
-            # llamar de nuevo a agente para que reponga en la mesa dos cosas al azar
+        semaforoFumadorConPapel.acquire()
+        logging.info('Armo cigarrillo')
+        time.sleep(1)
+        logging.info('Fumo cigarrillo')
+        time.sleep(1)
+        semaforoAgente.release()
 
 def fumadorConTabaco():
     while True:
-        # si hay fósforos y papel en la mesa
-            # tomarlos
-            # armar cigarrillo y fumar: se puede simular con un sleep
-            # llamar de nuevo a agente para que reponga en la mesa dos cosas al azar
+        semaforoFumadorConTabaco.acquire()
+        logging.info('Armo cigarrillo')
+        time.sleep(1)
+        logging.info('Fumo cigarrillo')
+        time.sleep(1)
+        semaforoAgente.release()
+
+def fumadorConFosforos():
+    while True:
+        semaforoFumadorConFosforos.acquire()
+        logging.info('Armo cigarrillo')
+        time.sleep(1)
+        logging.info('Fumo cigarrillo')
+        time.sleep(1)
+        semaforoAgente.release()
 
 
 agenteHilo = threading.Thread(target=agente)
 fumadorConPapelHilo = threading.Thread(target=fumadorConPapel)
-fumadorConFosforosHilo = threading.Thread(target=fumadorConFosforos)
 fumadorConTabacoHilo = threading.Thread(target=fumadorConTabaco)
+fumadorConFosforosHilo = threading.Thread(target=fumadorConFosforos)
 
 agenteHilo.start()
 fumadorConPapelHilo.start()
